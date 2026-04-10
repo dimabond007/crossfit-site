44617664a2e68991f5cf822b370a756e31f2cd59
dimabond007 <dmitriy@letstop.io>
2026-04-10 16:18:48 +0300

    test file boostapp

--- api/boostapp.ts
+++ api/boostapp.ts
@@ -1,17 +1,30 @@
-export default async function handler(): Promise<Response> {
-  try {
-    const API_KEY = process.env.BOOSTAPP_API_KEY;
+// export default async function handler(): Promise<Response> {
+//   try {
+//     const API_KEY = process.env.BOOSTAPP_API_KEY;
 
-    const res = await fetch("https://api.boostapp.co.il/v1/schedule", {
-      headers: {
-        Authorization: `Bearer ${API_KEY}`,
+//     const res = await fetch("https://api.boostapp.co.il/v1/schedule", {
+//       headers: {
+//         Authorization: `Bearer ${API_KEY}`,
+//       },
+//     });
+
+//     const data = await res.json();
+
+//     return Response.json({ data });
+//   } catch (e) {
+//     return Response.json({ error: "error" }, { status: 500 });
+//   }
+// }
+
+export default async function handler() {
+  return Response.json({
+    data: [
+      {
+        id: "1",
+        name: "Crossfit",
+        coach: "John",
+        start: new Date().toISOString(),
       },
-    });
-
-    const data = await res.json();
-
-    return Response.json({ data });
-  } catch (e) {
-    return Response.json({ error: "error" }, { status: 500 });
-  }
+    ],
+  });
 }

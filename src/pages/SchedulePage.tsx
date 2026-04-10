05420b4a02f077367f227adacf4ca9990f69d234
dimabond007 <dmitriy@letstop.io>
2026-04-10 17:35:17 +0300

    remove react import

--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { BoostappClass, ScheduleResponse } from '../types/types';
 
 export default function SchedulePage() {
